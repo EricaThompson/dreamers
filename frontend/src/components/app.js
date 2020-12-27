021ac9e0f03cd70e8572791498252c974c359259
joshkohane <joshkohane@gmail.com>
2020-12-27 13:27:28 -0500

    Setup home page

---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './home_page';
+
+const App = () => (
+    <div>
+        <HomePage />
+        WELCOME TO THE APP
+    </div>
+);
+
+export default App;
---
+++ frontend/src/components/home_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <h1>THIS IS THE HOMEPAGE</h1>
+        )
+    }
+}
+
+export default HomePage;
