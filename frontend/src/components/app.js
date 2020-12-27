50c2f47b098a4a75eaa5e137f0bab2fc8a1d0ea0
joshkohane <joshkohane@gmail.com>
2020-12-27 17:06:57 -0500

    Setup for commit

---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './home_page';
+import "../App.scss";
+
+const App = () => (
+    <div className="main-app" >
+        <HomePage />
+    </div>
+);
+
+export default App;
---
+++ frontend/src/components/home_page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../App.scss";
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="logo" >
+                {/* <i class="fas fa-cloud logo-icon"></i> */}
+                <h1 className="logo-title">DREAMERS</h1>
+                {/* <div className="box"></div> */}
+            </div>
+        )
+    }
+}
+
+export default HomePage;
