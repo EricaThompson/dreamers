2c9b69fe2a92f6a0fde92a7fef606d7304601b33
“ericathompson” <ericathompson.nc@gmail.com>
2020-12-28 10:58:42 -0800

    Restructure app.js for nav bar, start building and styling

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import NavBar from './nav/nav_bar';
 import HomePage from './home_page';
 import "../App.scss";
 
 const App = () => (
-    <div className="main-app" >
-        <HomePage />
+    <div className="app-container">
+        <div className="nav-bar">
+            <NavBar />
+        </div>
+        <div className="main-app" >
+            <HomePage />
+        </div>
     </div>
 );
 
