33d9e0d57575a1ef6bd9e1f3be361856585ddfdb
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-27 22:56:24 -0800

    Add body parser module and implement user registration endpoint

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI; 
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const users = require('./routes/api/users');
 const goals = require('./routes/api/goals');
 const dreams = require('./routes/api/dreams');
-const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -20,11 +22,13 @@ mongoose
     useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-
-
+  
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/api/users', users);
 app.use('/api/goals', goals);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,37 @@
 const express = require('express'); 
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 router.get('/test', (req, res) => {
     res.json({ msg: 'This is the user route'})
 });
+
+router.post('/register', (req, res) => {
+  console.log(req.body)
+  User.findOne({ username: req.body.username })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({username: "A user has already registered with this username"})
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          password: req.body.password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
+
 
 module.exports = router; 
