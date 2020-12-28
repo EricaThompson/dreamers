b78c859450907634758deb9744ca00fbe496e967
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-28 15:14:38 -0800

    Implement dream creation POST endpoint

--- routes/api/dreams.js
+++ routes/api/dreams.js
@@ -8,6 +8,26 @@ const validateDreamInput = require('../../validation/dreams');
 
 // create
 
+router.post('/',
+    passport.authenticate('jwt', { session: false}),
+    (req, res) => {
+        const { errors, isValid } = validateDreamInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newDream = new Dream({
+            userId: req.user.id,
+            text: req.body.text,
+            tags: req.body.tags,
+            type: req.body.type
+        })
+
+        newDream.save().then(dream => res.json(dream));
+    }
+)
+
 // read
 
 // update
