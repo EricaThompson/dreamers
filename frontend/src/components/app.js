9921398031d6da69ae3d475ef6c97fb25895e8a7
cindy803 <sheeli.803@gmail.com>
2020-12-28 11:41:27 -0800

    reactchanges

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
 import HomePage from './home_page';
 import "../App.scss";
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <div className="main-app" >
         <HomePage />
+        <Switch>
+        <AuthRoute exact path="/" component={HomePage} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
     </div>
 );
 
