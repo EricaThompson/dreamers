bc52591fc4cdd219d842f48d09a045ca610fdae4
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-29 10:31:44 -0800

    Add new routes for reading (all, by id, or by user), updating, and deleting dreams

--- routes/api/dreams.js
+++ routes/api/dreams.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Dream = require('../../models/Dream');
-const validateDreamInput = require('../../validation/dreams');
+const validateCreateDreamInput = require('../../validation/create_dream');
+const validateUpdateDreamInput = require('../../validation/update_dream');
 
 // create
 
 router.post('/',
-    passport.authenticate('jwt', { session: false}),
+    passport.authenticate('jwt', { session: false }),
     (req, res) => {
-        const { errors, isValid } = validateDreamInput(req.body);
+        const { errors, isValid } = validateCreateDreamInput(req.body);
 
         if (!isValid) {
             return res.status(400).json(errors);
@@ -30,8 +31,86 @@ router.post('/',
 
 // read
 
+router.get('/', (req, res) => {
+    Dream.find()
+        .sort({ date: -1})
+        .then(dreams => res.json(dreams))
+        .catch(err => res.status(400).json({ nodreamsfound: 'No dreams found'}));
+})
+
+router.get('/user/:userId', (req, res) => {
+    Dream.find({userId: req.params.userId})
+        .then(dreams => res.json(dreams))
+        .catch(err =>
+            res.status(404).json({ nodreamsfound: 'No dreams found from that user' }
+        )
+    );
+});
+
+router.get('/:id', (req, res) => {
+    Dream.find({userId: req.params.id})
+        .then(dreams => res.json(dreams))
+        .catch(err =>
+            res.status(404).json({ nodreamsfound: 'No dreams with that ID' }
+        )
+    );
+});
+
 // update
 
+router.post('/update/:dreamId',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateUpdateDreamInput(req.body);
+
+        if (!isValid) {
+            return res.json.status(400).json(errors);
+        }
+
+        var query = { _id: req.params.dreamId }
+        Dream.findOne(query)
+            .then(dream => {
+                if (dream.userId != req.user.id) {
+                    res.status(400).json({ userauth: 'You can only edit your own dreams'})
+                } else {
+                    if (req.body.text.length === 0) {
+                        delete req.body.text;
+                    }
+                    Dream.update(query, { $set: req.body }, function(err) {
+                        if(err) {
+                            res.status(400).json(err);
+                        } else {
+                            res.json(req.params.dreamId);
+                        }
+                    })
+                }
+            })
+            .catch(err => res.status(404).json({ nodreamfound: 'No dream found with that ID' }))
+
+    }
+)
+
 // delete
+
+router.delete('/:dreamId',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        var query = { _id: req.params.dreamId }
+        Dream.findOne(query)
+            .then(dream => {
+                if (dream.userId != req.user.id) {
+                    res.status(400).json({ userauth: 'You can only delete your own dreams'})
+                } else {
+                    Dream.deleteOne(query, function (err) {
+                        if(err) {
+                            res.status(400).json(err);
+                        } else {
+                            res.json(req.params.dreamId);
+                        }
+                    })
+                }
+            })
+    }
+)
 
 module.exports = router; 
