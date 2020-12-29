adb026db06780a4d1e63b520cb60780693a6f389
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-29 14:52:49 -0800

    Implement comment read GET requests by dream, by user, and by id

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -8,6 +8,7 @@ const Comment = require('../../models/Comment');
 const validateCommentInput = require('../../validation/comments');
 
 // create
+
 router.post('/:dreamId',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
@@ -31,18 +32,30 @@ router.post('/:dreamId',
             res.status(404).json({err})
           )
     }
-)
+);
 
 // read
 
-route.get('/:dreamId', (req, res) => {
-  Comment.find({dreamId: dreamId})
+router.get('/dream/:dreamId', (req, res) => {
+  Comment.find({dreamId: req.params.dreamId})
     .sort({date: -1})
     .then(comments => res.json(comments))
     .catch(err => res.status(404).json({ nocommentsfound: 'This dream has no comments'}))
-})
-// by id
-// by user
+});
+
+router.get('/user/:userId', (req, res) => {
+  Comment.find({userId: req.params.userId})
+    .sort({date: -1})
+    .then(comments => res.json(comments))
+    .catch(err => res.status(404).json({ nocommentsfound: 'This user has no comments'}))
+});
+
+router.get('/:id', (req, res) => {
+  Comment.findOne({_id: req.params.id})
+    .sort({date: -1})
+    .then(comment => res.json(comment))
+    .catch(err => res.status(404).json({ nocommentfound: 'No comment found with the specified id'}))
+});
 
 // update
 
