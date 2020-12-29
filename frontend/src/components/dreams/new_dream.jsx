980139c25175da685848a2f78fcf7cbe0cc90876
joshkohane <joshkohane@gmail.com>
2020-12-29 14:43:49 -0500

    Add modal style

--- frontend/src/components/dreams/new_dream.jsx
+++ frontend/src/components/dreams/new_dream.jsx
@@ -1,52 +1,91 @@
 import React from 'react';
 
 class NewDream extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedOption: 'dream',
+            dreamText: '',
+            searchValue: '',
+            tags: ['KillingIt', 'Love', 'Teeth'],
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(type) {
+        return (e) => {
+            this.setState({ [type]: e.target.value })
+        }
+    }
+
+    handleTags(e) {
+        let newTags = this.state.tags
+        newTags.push(e.target.value)
+        this.setState({ tags: newTags })
+    }
+
     render() {
         return (
-            <div>
-                <div className="radiobutton">
-                    <label>Dream
+            <div className="new-dream-container" >
+                <h1 className="new-dream-header" >New Dream</h1>
+                <form className="new-dream-radio">
+                    <label className="new-dream-radio-btn" >
+                        <h1 className={this.state.selectedOption === 'dream' ? "new-dream-radio-btn-header-checked" : "new-dream-radio-btn-header"} >Dream</h1>
                         <input
                             type="radio"
-                            value="Dream"
-                            // checked={this.state.selectedOption === "Dream"}
-                            onChange={this.handleChange}
+                            value="dream"
+                            checked={this.state.selectedOption === "dream"}
+                            onChange={() => this.handleChange('selectedOption')}
                         />
                     </label>
-                </div>
-                <div className="radio">
-                    <label>Goal
+                    <label className="new-dream-radio-btn" >
+                        <h1 className={this.state.selectedOption === 'goal' ? "new-dream-radio-btn-header-checked" : "new-dream-radio-btn-header"} >Goal</h1>
                         <input
                             type="radio"
-                            value="Goal"
-                            // checked={this.state.selectedOption === "Goal"}
-                            onChange={this.handleChange}
+                            value="goal"
+                            checked={this.state.selectedOption === "goal"}
+                            onChange={() => this.handleChange('selectedOption')}
                         />
                     </label>
+                </form>
+                <div className="new-dream-tags-container" >
+                    <h1 className="new-dream-tags-header" >Tags:</h1>
+                    <div className="new-dream-tags" >
+                        {this.state.tags.map((tag, idx) => {
+                            return (
+                                <div className="new-dream-tags-item-container" >
+                                    <div className="new-dream-tags-item-circle" ></div>
+                                    <p key={idx} className="new-dream-tags-item" >{tag}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
                 <div className="create-dream-container" >
                     <div className="create-dream-search-container" >
-                        <i className="fas fa-search feed-search-icon"></i>
-                        <form className="create-dream--search-form" >
+                        <i className="fas fa-search dream-search-icon"></i>
+                        <form className="create-dream-search-form" >
                             <input type="text"
-                                placeholder="Search tags, add tags, etc..."
-                                // value={this.state.selectedOption}
-                                onChange={this.handleChange}
+                                placeholder="Add tags or create a new one..."
+                                value={this.state.searchValue}
+                                onChange={() => this.handleChange('searchValue')}
                                 className="create-dream-search-input" />
                         </form>
                     </div>
                 </div>
                 <div className="create-dream-container" >
-                    <div className="create-dream-dreams" >
-                        <p className="create-dream-text" >Dream Text</p>
-                    </div>
+                    <form className="create-dream-text-container" >
+                        <textarea 
+                            className="create-dream-textarea" 
+                            placeholder="Share your dreams..."
+                            value={this.state.dreamText}
+                            onChange={() => this.handleChange('dreamText')}></textarea>
+                    </form>
                 </div>
-                <div>
-                    {/* Selected option is : {this.state.selectedOption} */}
+                <div className="create-dream-btn" >
+
+                    <input className="new-dream-btn create-dream-btn" type="submit" value="Create Dream"/>
                 </div>
-                <button className="btn btn-default" type="Create Dream">
-                Submit
-                </button>
             </div>
         )
     }
--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -4,22 +4,22 @@ import { connect } from 'react-redux';
 import NewDreamContainer from '../dreams/new_dream_container';
 
 const Modal = ({ modal, closeModal }) => {
-    if (!modal) {
-        return null;
-    }
-    let component;
-    switch (modal) {
-        case 'newDream':
-            component = <NewDreamContainer />;
-            break;
-        default:
-            return null;
-    }
+    // if (!modal) {
+    //     return null;
+    // }
+    // let component;
+    // switch (modal) {
+    //     case 'newDream':
+    //         component = <NewDreamContainer />;
+    //         break;
+    //     default:
+    //         return null;
+    // }
     return (
         <div className="modal-background" onClick={closeModal} >
             <div className="modal-child" onClick={e => e.stopPropagation()} >
-                { component }
-                {/* <NewDreamContainer /> */}
+                {/* { component } */}
+                <NewDreamContainer />
             </div>
         </div>
     )
