13780ac478b4616e884ffcc0d4c6258f30b2c0e2
joshkohane <joshkohane@gmail.com>
2020-12-29 16:42:19 -0500

    Connect profile route to container

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -4,7 +4,8 @@ import { logout } from '../../actions/session_actions';
 import NavBar from './navbar';
 
 const mapStateToProps = state => ({
-    loggedIn: state.session.isAuthenticated
+    loggedIn: state.session.isAuthenticated,
+    currentUser: state.session.user,
 });
 
 export default connect(
