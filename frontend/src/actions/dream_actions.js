21cdbfafc3ba6ecf6c9ef6d753ebf372ae13d6c8
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-30 16:18:24 -0800

    Refactor dream actions and reducers to simplify dream slice of redux store

--- frontend/src/actions/dream_actions.js
+++ frontend/src/actions/dream_actions.js
@@ -1,23 +1,17 @@
 import * as DreamApiUtils from '../util/dream_api_util';
 
-export const RECEIVE_ALL_DREAMS = 'RECEIVE_ALL_DREAMS';
-export const RECEIVE_USER_DREAMS = 'RECEIVE_USER_DREAMS';
-export const RECEIVE_NEW_DREAM = 'RECEIVE_NEW_DREAM';
+export const RECEIVE_DREAMS = 'RECEIVE_DREAMS';
+export const RECEIVE_DREAM = 'RECEIVE_DREAM';
 export const REMOVE_DREAMS = 'REMOVE_DREAMS';
+export const CLEAR_DREAMS = 'CLEAR_DREAMS';
 
-
-export const receiveAllDreams = (dreams) => ({
-    type: RECEIVE_ALL_DREAMS, 
+export const receiveDreams = (dreams) => ({
+    type: RECEIVE_DREAMS, 
     dreams
 });
 
-export const receiveUserDreams = (dreams) => ({
-    type: RECEIVE_USER_DREAMS,
-    dreams 
-});
-
-export const receiveNewDream = (dream) => ({
-    type: RECEIVE_NEW_DREAM,
+export const receiveDream = (dream) => ({
+    type: RECEIVE_DREAM,
     dream
 });
 
@@ -26,50 +20,53 @@ export const removeDreams = (dreamId) => ({
     dreamId
 })
 
+export const clearDreams = () => ({
+    type: CLEAR_DREAMS,
+})
 
 export const fetchDreams = () => dispatch => (
     DreamApiUtils.fetchDreams()
-    .then(dreams => dispatch(receiveAllDreams(dreams)))
+    .then(dreams => dispatch(receiveDreams(dreams.data)))
     .catch(err => console.log(err))
 )
 
 export const fetchDreamsByUser = userId => dispatch => (
     DreamApiUtils.fetchDreamsByUser(userId)
-    .then(dreams => dispatch(receiveUserDreams(dreams)))
+    .then(dreams => dispatch(receiveDreams(dreams.data)))
     .catch(err => console.error(err))
 )
 
 export const fetchDreamById = dreamId => dispatch => (
     DreamApiUtils.fetchDreamById(dreamId)
-    .then(dream => dispatch(receiveNewDream(dream)))
+    .then(dream => dispatch(receiveDream(dream.data)))
     .catch(err => console.error(err))
 )
 
 export const fetchDreamsByType = type => dispatch => (
     DreamApiUtils.fetchDreamsByType(type)
-    .then(dreams => dispatch(receiveAllDreams(dreams)))
+    .then(dreams => dispatch(receiveDreams(dreams.data)))
     .catch(err => console.error(err))
 )
 
 export const fetchDreamsByTags = tags => dispatch => (
     DreamApiUtils.fetchDreamsByTags(tags)
-    .then(dreams => dispatch(receiveAllDreams(dreams)))
+    .then(dreams => dispatch(receiveDreams(dreams.data)))
     .catch(err => console.error(err))
 )
 
 export const createDream = dream => dispatch => (
     DreamApiUtils.createDream(dream)
-    .then(dream => dispatch(receiveNewDream(dream)))
+    .then(dream => dispatch(receiveDream(dream.data)))
     .catch(err => console.error(err))
 )
 
 export const updateDream = (dreamId, updatedFields) => dispatch  => (
     DreamApiUtils.updateDream(dreamId, updatedFields)
-    .then((dreamId, updatedFields)=> dispatch(receiveNewDream(dreamId, updatedFields)))
+    .then((dreamId, updatedFields)=> dispatch(receiveDream(dreamId, updatedFields)))
     .catch(err => console.error(err))
 )
 
 export const deleteDream = dreamId => dispatch => (
     DreamApiUtils.deleteDream(dreamId)
-    .then(() => dispatch(removeDreams(dreamId)))
+    .then(dreamId => dispatch(removeDreams(dreamId)))
 )
