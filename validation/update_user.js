38482520a91cae57297e1407fced045c48036150
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-30 20:48:20 -0800

    Update user info validations to remove minimum length - allow for empty bio and age

--- validation/update_user.js
+++ validation/update_user.js
@@ -6,11 +6,11 @@ module.exports = function validateUpdateUserInput(data) {
 
   data.bio = validText(data.bio) ? data.bio : '';
 
-  if (data.bio && !Validator.isLength(data.bio, { min: 1, max: 100 })) {
+  if (data.bio && !Validator.isLength(data.bio, { max: 100 })) {
     errors.bio = 'Bio cannot be longer than 100 characters';
   }
 
-  if (data.age && !Validator.isLength(data.age, { min: 1, max: 10 })) {
+  if (data.age && !Validator.isLength(data.age, { max: 10 })) {
     errors.age = "Age cannot be longer than 10 characters";
   }
 
