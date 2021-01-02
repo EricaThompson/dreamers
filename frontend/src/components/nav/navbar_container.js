bc50d14420fe50897eda2aa58cb0b40ba9e10bf9
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-02 15:56:15 -0800

    Fix logout

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -12,7 +12,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    logout: logout,
+    logout: ()=>dispatch(logout()),
     openModal: (type)=> dispatch(openModal(type))
 
 })
