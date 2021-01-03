609b5142e59d5056c9de943882fd260483891729
cindy803 <sheeli.803@gmail.com>
2021-01-02 19:45:57 -0800

    clean the code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,17 +5,6 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
-<<<<<<< HEAD
-import { createComment } from './actions/comment_actions';
-
-import { 
-  fetchLike,
-  createLike, 
-  deleteLike
-} from './actions/like_actions'
-
-=======
->>>>>>> 6a9c9e24b91c583f86c54b708e2845472d882f09
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -40,16 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore({});
   }
 
-<<<<<<< HEAD
-  window.store = store 
-  window.fetchLike = fetchLike
-  window.createLike = createLike
-  window.deleteLike = deleteLike
 
-
-
-=======
->>>>>>> 6a9c9e24b91c583f86c54b708e2845472d882f09
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -5,7 +5,6 @@ import ui from './ui_reducer';
 import dream from './dream_reducer';
 import comment from './comment_reducer';
 import user from './user_reducer';
-import tag from './tag_reducers';
 import search from './search_reducer';
 import modalInfo from './modal_info_reducer';
 import tag from './tag_reducers'; 
@@ -18,15 +17,9 @@ const RootReducer = combineReducers({
     dream, 
     comment,
     user,
-<<<<<<< HEAD
     modalInfo, 
     tag, 
     like
-=======
-    tag,
-    search,
-    modalInfo,
->>>>>>> 6a9c9e24b91c583f86c54b708e2845472d882f09
 });
 
 export default RootReducer;
