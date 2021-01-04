e28f20753c0c524a0faf63e193529712d9531ee9
alanxudai@gmail.com <alanxudai@gmail.com>
2021-01-03 16:38:26 -0800

    Fix follow action typos

--- frontend/src/actions/follow_actions.js
+++ frontend/src/actions/follow_actions.js
@@ -1,5 +1,7 @@
-import * as FollowApiUtil from '../util/dream_api_util';
+import * as FollowApiUtil from '../util/follow_api_util';
 import { receiveUser } from './user_actions';
+
+export const RECEIVE_FOLLOW_USERS = 'RECEIVE_FOLLOW_USERS';
 
 export const receiveFollowUsers = (users) => ({
   type: RECEIVE_FOLLOW_USERS,
@@ -18,13 +20,13 @@ export const fetchFollowers = userId => dispatch => (
   .catch(err => console.log(err))
 )
 
-export const followUser = userId = dispatch => (
+export const followUser = userId => dispatch => (
   FollowApiUtil.followUser(userId)
   .then(payload => dispatch(receiveUser(payload.data)))
   .catch(err => console.log(err))
 )
 
-export const unfollowUser = userId = dispatch => (
+export const unfollowUser = userId => dispatch => (
   FollowApiUtil.unfollowUser(userId)
   .then(payload => dispatch(receiveUser(payload.data)))
   .catch(err => console.log(err))
