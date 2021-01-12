6de38bbc7cbb8df7b9b1bd239995787820ce3d11
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-11 21:55:47 -0800

    Style view of followers from profile

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -286,7 +286,7 @@ class Profile extends React.Component {
                         </div>
                         <div onClick={this.handlePopOut}>Followers: {this.state.followers.length}</div>
                         {this.state.popout ?
-                            <div className="likes-popout-container" onClick={(e) => e.stopPropagation()} >
+                            <div className="follower-popout-container" onClick={(e) => e.stopPropagation()} >
                                 <div>{popout}</div>
                             </div>
                             : ''}
