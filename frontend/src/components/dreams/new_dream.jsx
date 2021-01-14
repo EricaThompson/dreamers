0db556dafccf76741705771d1a570944a2204985
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-13 19:19:37 -0800

    Break long lines in new_dream.jsx

--- frontend/src/components/dreams/new_dream.jsx
+++ frontend/src/components/dreams/new_dream.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class NewDream extends React.Component {
     constructor(props) {
         super(props);
-        if (this.props.info === null || Object.values(this.props.info).length === 0) {
-
+        if (this.props.info === null 
+            || Object.values(this.props.info).length === 0) {
             this.state = {
                 selectedOption: 'dream',
                 dreamText: '',
@@ -105,9 +105,17 @@ class NewDream extends React.Component {
                 this.state.tags.map((tag, idx) => {
                     if (tag != null) {
                         return (
-                            <div key={idx} className="new-dream-tags-item-container" onClick={this.removeTag(tag)}>
-                                <div className="new-dream-tags-item-circle" ></div>
-                                <p className="new-dream-tags-item" >{tag}</p>
+                            <div key={idx} 
+                                className="new-dream-tags-item-container" 
+                                onClick={this.removeTag(tag)}
+                            >
+                                <div 
+                                    className="new-dream-tags-item-circle" >
+
+                                </div>
+                                <p 
+                                    className="new-dream-tags-item" >{tag}
+                                </p>
                             </div>
                         )
                     } else {
@@ -122,11 +130,22 @@ class NewDream extends React.Component {
         if (Object.values(this.props.searchResults).length > 0) {
 
             search = <div>
-                {Object.values(this.props.searchResults.tags).map((result, idx) => (
-                    <div className="tag-search-results-outer-container" >
-                        <div className="tag-search-results-inner-container" onClick={() => this.handleTags(result.name)} key={idx} >
-                            <i class="fas fa-tag search-icon"></i>
-                            {result.name}
+                        {Object.values(this.props.searchResults.tags)
+                            .map((result, idx) => (
+                                <div 
+                                    className=
+                                        "tag-search-results-outer-container" 
+                                >
+                                    <div 
+                                        className=
+                                            "tag-search-results-inner-container" 
+                                        onClick={
+                                            () => this.handleTags(result.name)} 
+                                        key={idx} 
+                                    >
+                                        <i class="fas fa-tag search-icon">
+                                        </i>
+                                        {result.name}
                         </div>
                     </div>
                 ))}
@@ -136,11 +155,29 @@ class NewDream extends React.Component {
         }
 
         return (
-            <div className="new-dream-container" onClick={e => e.stopPropagation()} >
-                <h1 className="new-dream-header" >{this.props.info === null || Object.values(this.props.info).length === 0 ? "New Dream" : "Edit Dream"}</h1>
-                <form className="new-dream-radio">
-                    <label className="new-dream-radio-btn" >
-                        <h1 className={this.state.selectedOption === 'dream' ? "new-dream-radio-btn-header-checked" : "new-dream-radio-btn-header"} >Dream</h1>
+            <div 
+                className="new-dream-container" 
+                onClick={e => e.stopPropagation()} 
+            >
+                <h1 
+                    className="new-dream-header" >{this.props.info === null 
+                    || Object.values(this.props.info).length === 0 
+                    ? "New Dream" 
+                    : "Edit Dream"
+                    }
+                </h1>
+                <form 
+                    className="new-dream-radio">
+                    <label 
+                        className="new-dream-radio-btn" >
+                        <h1 
+                            className={
+                                this.state.selectedOption === 'dream' 
+                                ? "new-dream-radio-btn-header-checked" 
+                                : "new-dream-radio-btn-header"} 
+                        >
+                                    Dream
+                        </h1>
                         <input
                             type="radio"
                             value="dream"
@@ -149,7 +186,14 @@ class NewDream extends React.Component {
                         />
                     </label>
                     <label className="new-dream-radio-btn" >
-                        <h1 className={this.state.selectedOption === 'goal' ? "new-dream-radio-btn-header-checked" : "new-dream-radio-btn-header"} >Goal</h1>
+                        <h1 
+                            className={
+                                this.state.selectedOption === 'goal' 
+                                    ? "new-dream-radio-btn-header-checked" 
+                                    : "new-dream-radio-btn-header"} 
+                        >
+                                        Goal
+                        </h1>
                         <input
                             type="radio"
                             value="goal"
@@ -164,22 +208,41 @@ class NewDream extends React.Component {
                         {tags}
                     </div>
                 </div>
-                <div className="create-dream-container" onClick={e => e.stopPropagation()} >
+                <div 
+                    className="create-dream-container" 
+                    onClick={e => e.stopPropagation()} 
+                >
                     <div className="create-dream-search-container" >
                         <i className="fas fa-search dream-search-icon"></i>
                         <form 
                             className="create-dream-search-form" 
                             onSubmit={()=>this.handleTags()}
                         >
-                                <input type="text"
-                                    placeholder="Search tags or create a new one"
-                                    value={this.state.searchValue}
-                                    onChange={this.handleSearchChange}
-                                    className="create-dream-search-input" 
-                                />
+                            <input type="text"
+                                placeholder="Search tags or create a new one"
+                                value={this.state.searchValue}
+                                onChange={this.handleSearchChange}
+                                className="create-dream-search-input" 
+                            />
                         </form>
-                        <i onClick={this.addTag} onSubmit={e => e.stopPropagation()} className={this.state.showClose ? "fas fa-check-circle search-check-btn" : ''}></i>
-                        <i onClick={this.hideShow} onSubmit={e => e.stopPropagation()} className={this.state.showClose ? "fas fa-times-circle close-search-btn" : ''}></i>
+                        <i onClick={this.addTag} 
+                            onSubmit={e => e.stopPropagation()} 
+                            className={
+                                this.state.showClose 
+                                    ? "fas fa-check-circle search-check-btn" 
+                                    : ''
+                            }
+                        >
+                        </i>
+                        <i 
+                            onClick={this.hideShow} 
+                            onSubmit={e => e.stopPropagation()} 
+                            className={
+                                this.state.showClose 
+                                ? "fas fa-times-circle close-search-btn" 
+                                : ''}
+                        >
+                        </i>
                     </div>
                     {search}
                 </div>
@@ -189,14 +252,20 @@ class NewDream extends React.Component {
                             className="create-dream-textarea" 
                             placeholder="Share your dreams..."
                             value={this.state.dreamText}
-                            onChange={this.handleChange('dreamText')}></textarea>
+                            onChange={this.handleChange('dreamText')}>
+                        </textarea>
                     </form>
                 </div>
 
                 <div className="create-dream-btn" >
                     <input className="new-dream-btn" 
                         type="submit" 
-                        value={this.props.info === null || Object.values(this.props.info).length === 0 ? "Create Dream" : "Edit Dream"}
+                        value={
+                            this.props.info === null 
+                            || Object.values(this.props.info).length === 0 
+                            ? "Create Dream" 
+                            : "Edit Dream"
+                        }
                         onClick={this.handleSubmit} 
                         />
                 </div>
