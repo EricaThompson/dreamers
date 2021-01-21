ea1f2afd4271d8261dd8267950bac4de258985a4
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-21 10:02:09 -0800

    Add fix for cannot GET/URL error

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const search = require('./routes/api/search');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', '/frontend/public/index.html'));
+    res.sendFile(
+      path.resolve(__dirname, 'frontend', 'build', '/frontend/public/index.html'));
   })
 }
 
@@ -48,4 +49,11 @@ app.use('/api/search', search);
 
 const port = process.env.PORT || 5000; 
 
+
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
+
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
